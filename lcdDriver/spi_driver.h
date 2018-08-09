dd94d5bf05806787657383794d5eb9f076aed3d5
Gabi <gabibraso@gmail.com>
2018-08-09 14:30:00 +0300

    I uploaded the lcd and spi drivers files.

---
+++ lcdDriver/spi_driver.h
@@ -0,0 +1,22 @@
+#ifndef _SPI_DRIVER_H_
+	#define _SPI_DRIVER_H_
+	
+#include <avr/io.h>
+
+
+typedef struct{
+ 	volatile uint8_t *spiPort;
+ 	unsigned char SSPin;
+ 	unsigned char SCKPin;
+ 	unsigned char MOSIPin;
+	unsigned char MISOPin;
+ } spiPins;
+
+spiPins spiInit;
+
+void spiStructInit(volatile uint8_t *Port, unsigned char SSPin, unsigned char SCKPin, unsigned char MOSIPin, unsigned char MISOPin);
+void spiPortInit();
+
+
+
+#endif
