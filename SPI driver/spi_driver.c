3627b71a12a76db56b6eb646b744e6937bd0e2a4
Gabi <gabibraso@gmail.com>
2018-08-30 13:09:26 +0300

    Updated SPI driver - interrupt - read/write

--- SPI driver/spi_driver.c
+++ SPI driver/spi_driver.c
@@ -1,69 +1,202 @@
 #include "spi_driver.h"
+#include "lcd_driver.h"
 #include <avr/io.h>
 #include <stdbool.h>
 
-volatile bool mosiState = false;
-volatile bool misoState = false;
 
-
-
-void spiStructInit(volatile uint8_t *Port, unsigned char SSPin, unsigned char SCKPin, unsigned char MOSIPin, unsigned char MISOPin)
+/**
+ * \brief			 This method is used to initialize the SPI pins used
+ * 
+ * \param Port		- Reference to the port that providing the SPI inputs
+ * \param SSPin		- Reference to the SS (Slave Select) pin used to select the Slave device
+ * \param SCKPin	- Reference to the SCK (CLOCK) pin used to generate the clock signal
+ * \param MOSIPin	- Reference to the MOSI (Master Output Slave Input) pin used to transmit
+					  data from Master to Slave
+ * \param MISOPin	- Reference to MISO (Master Input Slave Output) pin used to transmit
+					  data from Slave to Master
+ * \param dataOrder - Reference to 
+ * \param spiMode   -
+ * \param divider   -			  
+ * 
+ * \return void
+ */
+void spi_Init(	volatile uint8_t *Port, 
+				unsigned char SSPin, 
+				unsigned char SCKPin, 
+				unsigned char MOSIPin, 
+				unsigned char MISOPin,
+				uint8_t dataOrder,
+				uint8_t spiMode,
+				uint8_t divider
+				)
 {
-	spiInit.spiPort = Port;
-	spiInit.SSPin = SSPin;
-	spiInit.SCKPin = SCKPin;
-	spiInit.MOSIPin = MOSIPin;
-	spiInit.MISOPin = MISOPin;
+	spiStruct.spiPort = Port;
+	spiStruct.SSPin = SSPin;
+	spiStruct.SCKPin = SCKPin;
+	spiStruct.MOSIPin = MOSIPin;
+	spiStruct.MISOPin = MISOPin;
 		
-	if(spiInit.MOSIPin == '\x00')
+	if(spiStruct.MOSIPin == '\x00')
 	{
 		// put in HIGH Z state
-		*(spiInit.spiPort-1) &= ~(1<<spiInit.MOSIPin);
-		*(spiInit.spiPort) &= ~(1<<spiInit.MOSIPin);
+		*(spiStruct.spiPort-1) &= ~(1<<spiStruct.MOSIPin);
+		*(spiStruct.spiPort) &= ~(1<<spiStruct.MOSIPin);
 	}else
-	{
-		mosiState = true;
-	}
-	
-	if(spiInit.MISOPin == '\x00')
-	{
-		// put in HIGH Z state
-		*(spiInit.spiPort-1) &= ~(1<<spiInit.MISOPin);
-		*(spiInit.spiPort) &= ~(1<<spiInit.MISOPin);
-	}else
-	{
-		misoState = true;
-	}
-}
-
-void spiPortInit()
-{
-	//DDR - set DDR and make SCK, SS output pin
-	*(spiInit.spiPort-1) |= (1<<spiInit.SSPin)|(1<<spiInit.SCKPin);
-	
-	//DDR - make MOSI pin as output
-	if(mosiState)
 	{
 		//make it as output
-		*(spiInit.spiPort-1) |= (1<<spiInit.MOSIPin);
+		*(spiStruct.spiPort-1) |= (1<<spiStruct.MOSIPin);
 	}
 	
-	//DDR - make MISO pin as input
-	if(misoState)
+	if(spiStruct.MISOPin == '\x00')
+	{
+		// put in HIGH Z state
+		*(spiStruct.spiPort-1) &= ~(1<<spiStruct.MISOPin);
+		*(spiStruct.spiPort) &= ~(1<<spiStruct.MISOPin);
+	}else
 	{
 		//make it as input
-		*(spiInit.spiPort-1) &= ~(1<<spiInit.MISOPin);
+		*(spiStruct.spiPort-1) &= ~(1<<spiStruct.MISOPin);
 	}
 	
+	//DDR - set DDR and make SCK, SS output pin
+	*(spiStruct.spiPort-1) |= (1<<spiStruct.SSPin)|(1<<spiStruct.SCKPin);
+	
 	//set the SS pin to HIGH
-	*(spiInit.spiPort) |=  (1<<spiInit.SSPin);
-	*(spiInit.spiPort) &=  ~(1<<spiInit.SCKPin);
+	*(spiStruct.spiPort) |=  (1<<spiStruct.SSPin);
+	*(spiStruct.spiPort) &=  ~(1<<spiStruct.SCKPin);
 	
 	//Set SPI
-	SPCR = (1<<SPE) | (1<<MSTR) | (0<<SPR1) | (0<<SPR0);
-	SPSR = (1<<SPI2X);
+	SPCR |= (1<<SPE);
+	SPSR &= ~(1<<SPIE);
+	setSpiBitOrder(dataOrder);
+	setSpiMode(spiMode);
+	setSpiClockRate(divider);
+	
+	
+	
 }
 
+/**
+ * \brief				This method is used to select the first bit to be transmitted
+ * 
+ * \param dataOrder   - When this parameter take LSB_FIRST, the LSB of data word is transmitted first
+					  - When this parameter take MSB_FIRST, the MSB of data word is transmitted first
+ * 
+ * \return void
+ */
+void setSpiBitOrder(uint8_t dataOrder)
+{
+	if(dataOrder == LSB_FIRST)
+	{
+		SPCR |= (1<<DORD);
+	}
+	
+	if(dataOrder == MSB_FIRST)
+	{
+		SPCR &= ~(1<<DORD);
+	}
+}
 
+/**
+ * \brief		  This method is used to select SPI mode
+ *		
+ * \param mode	- This parameter can take two values: - MASTER_MODE -> the device is a master 
+ *													  - SLAVE_MODE -> the device is a slave
+ * \return void
+ */
+void setSpiMode(uint8_t spiMode)
+{
+	if(spiMode == MASTER_MODE)
+	{
+		SPCR |= (1<<MSTR);
+	}
+	
+	if(spiMode == SLAVE_MODE)
+	{
+		SPCR &= ~(1<<MSTR);
+	}
+}
+
+/**
+ * \brief			This method is used to select the SPI clock rate
+ * 
+ * \param divider	- this parameter take the clock divider value 
+ * 
+ * \return void
+ */
+void setSpiClockRate(uint8_t divider)
+{
+	if(divider == SPI_CLOCK_DIV2) {SPCR &= ~((1<<SPR0) | (1<<SPR1)); SPSR |= (1<<SPI2X);}
+	if(divider == SPI_CLOCK_DIV4) {SPCR &= ~((1<<SPR0) | (1<<SPR1)); SPSR &= ~(1<<SPI2X);}
+	if(divider == SPI_CLOCK_DIV8) {SPCR |= (1<<SPR0); SPCR &= ~(1<<SPR1); SPSR |= (1<<SPI2X);}
+	if(divider == SPI_CLOCK_DIV16) {SPCR |= (1<<SPR0); SPCR &= ~(1<<SPR1); SPSR &= ~(1<<SPI2X);}	
+	if(divider == SPI_CLOCK_DIV32) {SPCR |= (1<<SPR1); SPCR &= ~(1<<SPR0); SPSR |= (1<<SPI2X);}
+	if(divider == SPI_CLOCK_DIV64) {SPCR &= ~(1<<SPR0); SPCR |= (1<<SPR1); SPSR &= ~(1<<SPI2X);}	
+	if(divider == SPI_CLOCK_DIV128) {SPCR |= (1<<SPR0) | (1<<SPR1); SPSR &= ~(1<<SPI2X);}
+		
+}
+
+void spiWritePooling(uint8_t data)
+{
+	SPDR = data;
+	while((SPSR & (1<<SPIF)) == 0);
+}
+
+/**
+ * \brief					That method is used to enable the SPI interrupt and to transmit the first byte of data.
+							After the first byte was transmitted the ISR routine is executed.
+							SpiCounters is incremented by 1 (a byte was transmitted) and if this counter
+							is equal with the number of bytes that we want to transmit, transmission is
+							complete and then put the SS pin back to HIGH and reset the interrupt enable. 
+							If the counter is not equal with the the number of bytes that we want to transmit,
+							the SPDR register will be equal with the value of the sTxBuffer address + spiCounter (sTxBuffer + 1 for e.g.).
+							After the next byte was transmitted the ISR routine is executed again.
+							SpiCounter is incremented again by 1 (now his value will be 2) and as before if this counter is 
+							equal with the number of bytes that we want to transmit, transmission is complete and so on.
+							
+ * 
+ * \param sTxBuffer			- the Tx buffer that store the bits to be transmitted 
+ * \param sRxBuffer			- the Rx buffer that store received bits 
+ * \param nBytesToTransmit	- number of bytes that we want to transmit
+ * 
+ * \return void
+ */
+void spiInterruptTransmit(uint8_t *sTxBuffer, uint8_t *sRxBuffer, uint8_t nBytesToTransmit)
+{
+	bytesToTransmit = nBytesToTransmit;
+	spiTxBuffer = sTxBuffer;
+	spiRxBuffer = sRxBuffer;
+	spiStatus = SPI_IDLE;
+	// enable interrupt 
+	SPCR |= (1<<SPIE);
+	sei();
+		
+		
+	if(spiStatus == SPI_IDLE)
+	{
+		//Put SS to LOW (select this device)
+		*(spiStruct.spiPort) &= ~(1<<spiStruct.SSPin);
+		SPDR = *spiTxBuffer;
+		spiCounter = 0;
+		spiStatus = SPI_TRANSFER_ONGOING;
+	}
+}
+
+ISR(SPI_STC_vect)
+{
+	spiRxBuffer = SPDR;
+	spiCounter++;
+	if(bytesToTransmit == spiCounter)
+	{//transmission complete reset the interrupt enable
+		SPCR &= ~(1<<SPIE);
+		spiStatus = SPI_IDLE;
+	    // Put SS to HIGH (deselect this device)
+	   *(spiStruct.spiPort) |= (1<<spiStruct.SSPin);
+	}else
+	{
+		SPDR = *(spiTxBuffer + spiCounter);
+	}
+	
+}
 
 
--- SPI driver/spi_driver.h
+++ SPI driver/spi_driver.h
@@ -2,6 +2,7 @@
 	#define _SPI_DRIVER_H_
 	
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 
 typedef struct{
@@ -12,11 +13,49 @@ typedef struct{
 	unsigned char MISOPin;
  } spiPins;
 
-spiPins spiInit;
+spiPins spiStruct;
 
-void spiStructInit(volatile uint8_t *Port, unsigned char SSPin, unsigned char SCKPin, unsigned char MOSIPin, unsigned char MISOPin);
-void spiPortInit();
+#define SPI_IDLE                0x00
+#define SPI_TRANSFER_ONGOING    0x01
+#define LSB_FIRST				0x02
+#define MSB_FIRST				0x03
+#define MASTER_MODE				0x04
+#define SLAVE_MODE				0x05
+#define SPI_CLOCK_DIV2			0x06
+#define SPI_CLOCK_DIV4			0x07
+#define SPI_CLOCK_DIV8			0x08
+#define SPI_CLOCK_DIV16			0x09
+#define SPI_CLOCK_DIV32			0x10
+#define SPI_CLOCK_DIV64			0x11
+#define SPI_CLOCK_DIV128		0x12
 
 
 
+volatile uint8_t bytesToTransmit;
+volatile uint8_t bytesToReceive;
+volatile uint8_t spiCounter;
+volatile uint8_t *spiRxBuffer;
+volatile uint8_t *spiTxBuffer;
+volatile uint8_t spiStatus;
+
+uint32_t dataToTransmit;
+uint8_t dataLength ;
+uint32_t dataBuffer;
+
+void spi_Init(	volatile uint8_t *Port, 
+				unsigned char SSPin, 
+				unsigned char SCKPin, 
+				unsigned char MOSIPin, 
+				unsigned char MISOPin, 
+				uint8_t dataOrder,
+				uint8_t spiMode,
+				uint8_t divider
+				);
+
+void spiWritePooling(uint8_t data);
+
+void spiInterruptTransmit(uint8_t *sTxBuffer, uint8_t *sRxBuffer, uint8_t nBytesToTransmit);
+void setSpiBitOrder(uint8_t dataOrder);
+void setSpiMode(uint8_t spiMode);
+void setSpiClockRate(uint8_t divider);
 #endif
